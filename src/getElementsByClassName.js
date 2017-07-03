4c819dc54cd730a1062196eed5573ff11a01ea5d
Rick Gallegos <duulketariakan@yahoo.com>
2017-07-03 15:20:18 -0700

    Recursion Review - Get Elements By Class Name

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,34 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var results = []
+
+  var exploreClasses = function(results, node, className) {
+    // check classList of node for a match of className
+    //node.classList.contains(className)
+
+    if (node.classList && node.classList.contains(className)) {
+        results.push(node);
+    }
+
+    //check if there are more nodes under this node
+      //if there are more nodes, then call exploreClasses function on that child
+    if (node.childNodes) {
+      for (var j =0; j<node.childNodes.length; j++) {
+        exploreClasses(results, node.childNodes[j], className)
+      }
+    }
+
+
+
+  }
+
+  //call exploreClasses with document.body
+  exploreClasses(results, document.body, className)
+
+  return results
+
+
 };
