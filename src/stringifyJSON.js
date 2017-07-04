769a708cf63d563c56fd9166888ea657ed118c05
Rick Gallegos <duulketariakan@yahoo.com>
2017-07-03 20:32:43 -0700

    Recursion Review Stringify JSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'boolean') {
+    return obj + '';
+  } else if (typeof obj === 'number') {
+    return obj + '';
+  } else if (obj === null) {
+    return obj + '';
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'function') {
+    return undefined;
+  } else if (obj === undefined) {
+    return obj;
+  } else if (Array.isArray(obj)) {
+    var stringArray = [];
+    for (var i = 0; i < obj.length; i += 1) {
+      stringArray.push(stringifyJSON(obj[i]));
+    }
+    return "[" + stringArray.join(',') + "]";
+  } else if (typeof obj === 'object') {
+    var stringObj = '';
+    for (var key in obj) {
+      if (typeof key !== 'function' && typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        stringObj += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    stringObj = stringObj.slice(0, stringObj.length - 1);
+    return stringObj = '{' +  stringObj + '}';
+  }
 };
